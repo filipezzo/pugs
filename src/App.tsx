39d66d0eadf7b6946d2bd7f45fc78b348a0ba05e
Filipe Avanzzo <avanzzodev@gmail.com>
2024-06-17 17:29:50 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Router } from "./app/router/Router";
+const queryClient = new QueryClient();
+function App() {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Router />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
+}
+
+export default App;
---
+++ src/app/router/Router.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export function Router() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="">
+					<Route />
+					<Route />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
+}
